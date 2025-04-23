79e9891de836226c7106e7c71367afae64dc5e1c
Karsten Kroesch <karsten.kroesch@ag.ch>
2025-04-23 09:51:03 +0200

    Added Postgres database.

--- clickops/settings.py
+++ clickops/settings.py
@@ -75,9 +75,16 @@ WSGI_APPLICATION = "clickops.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "clickops",
+            "passfile": ".pgpass",
+        },
     }
 }
 
